041c56717b11bfbfdd976dcc59ad92d69a5ae1e2
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-05-18 21:17:14 +0530

    final

--- ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -15,7 +15,7 @@ namespace ContosoCrafts.WebSite.Models
         public string Image { get; set; }
         public string Url { get; set; }
         public string  Title { get; set; }
-        public string Discription { get; set; }
+        public string Description { get; set; }
         public int[] Ratings { get; set; }
 
         public override string ToString() =>   JsonSerializer.Serialize<Product>(this);
